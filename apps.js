832337cb30d3cbf751e18a8cd20b2d4f6248c3eb
sahil mulani <113999437+stranzerone@users.noreply.github.com>
2025-05-17 16:32:05 +0530

    inital check for inventory

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native"
 import WorkOrderPage from "./app/WorkOrders/WorkOrderScreen"
 import DashboardOptionsScreen from "./app/WorkOrderStack/WoHomeTab"
-
+// this is inventoru repo
  export default App = ()=>{
 <View className="flex">
 
