9acce569ca475f0dd2089d2ca8beb43bcfc6b9f1
sahil mulani <113999437+stranzerone@users.noreply.github.com>
2025-05-17 16:25:28 +0530

    changes commit check for offline

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native"
 import WorkOrderPage from "./app/WorkOrders/WorkOrderScreen"
 import DashboardOptionsScreen from "./app/WorkOrderStack/WoHomeTab"
-
+// this is offline code
  export default App = ()=>{
 <View className="flex">
 
